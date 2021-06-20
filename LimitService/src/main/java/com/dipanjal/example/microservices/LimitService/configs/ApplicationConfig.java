59b74f8310ec642f3bfa277d499a5bc3ea3b82e4
dipanjal <dipanjal.maitra@bs-23.net>
2021-06-20 22:16:12 +0600

    Config Server Profiling

--- LimitService/src/main/java/com/dipanjal/example/microservices/LimitService/configs/ApplicationConfig.java
+++ LimitService/src/main/java/com/dipanjal/example/microservices/LimitService/configs/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package com.dipanjal.example.microservices.LimitService.configs;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
