b85824051c99646b5be85f37d905a4f98ebf6d12
dipanjal <dipanjal.maitra@bs-23.net>
2021-06-22 11:47:03 +0600

    External Scrapper Project Add WIP

--- CurrencyExchangeService/src/main/java/com/dipanjal/example/microservices/CurrencyExchangeService/CurrencyExchangeApplication.java
+++ CurrencyExchangeService/src/main/java/com/dipanjal/example/microservices/CurrencyExchangeService/CurrencyExchangeApplication.java
@@ -2,8 +2,12 @@ package com.dipanjal.example.microservices.CurrencyExchangeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {
+//		"com.dipanjal.example.microservices.CurrencyExchangeService"
+//})
 public class CurrencyExchangeApplication {
 
 	public static void main(String[] args) {
--- CurrencyExchangeService/src/main/java/com/dipanjal/example/microservices/CurrencyExchangeService/service/CurrencyExchangeServiceScrap.java
+++ CurrencyExchangeService/src/main/java/com/dipanjal/example/microservices/CurrencyExchangeService/service/CurrencyExchangeServiceScrap.java
@@ -2,17 +2,39 @@ package com.dipanjal.example.microservices.CurrencyExchangeService.service;
 
 import com.dipanjal.example.microservices.CurrencyExchangeService.exception.CurrencyExchangeException;
 import com.dipanjal.example.microservices.CurrencyExchangeService.model.CurrencyExchange;
+import com.dipanjal.example.microservices.CurrencyExchangeService.props.CurrencyExchangeProps;
+import com.dipanjal.htmlunit.service.scrapper.ifaces.CurrencyExchangeScrapper;
+import com.dipanjal.htmlunit.service.scrapper.ifaces.WebScrapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 @Service("currencyExchangeServiceScrap")
+@RequiredArgsConstructor
 public class CurrencyExchangeServiceScrap implements CurrencyExchangeService {
 
-//    private final ImageScrapper glitterFlickDownloader;
+/*    private final WebScrapper<String, Double> currencyExchangeScrapper;
+
+    public CurrencyExchangeServiceScrap(WebScrapper<String, Double> currencyExchangeScrapper) {
+        this.currencyExchangeScrapper = currencyExchangeScrapper;
+    }*/
+
+    private final CurrencyExchangeScrapper<Double> currencyExchangeScrapper;
+    private final CurrencyExchangeProps props;
 
     @Override
     public CurrencyExchange fetchValue(String from, String to) throws CurrencyExchangeException {
-        return new CurrencyExchange(1L, from, to, new BigDecimal(65));
+        try{
+            Double exchangeValue = currencyExchangeScrapper.scrap(
+                    String.format(props.getApi().getUrl(), from, to),
+                    Double::valueOf
+            );
+            return new CurrencyExchange(1L, from, to, BigDecimal.valueOf(exchangeValue));
+        }catch (IOException e){
+            throw new CurrencyExchangeException("Scrapping Server Exception", e);
+        }
     }
 }
