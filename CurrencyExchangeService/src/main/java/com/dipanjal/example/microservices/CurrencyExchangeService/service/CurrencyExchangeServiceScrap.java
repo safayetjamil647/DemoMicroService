bf07d7b472e1aa4cbdfa19c5e358f4d1940c2d71
dipanjal <dipanjalmaitra@gmail.com>
2022-01-31 02:16:29 +0600

    HtmlUnit Scrapper Enabled

--- CurrencyExchangeService/src/main/java/com/dipanjal/example/microservices/CurrencyExchangeService/service/CurrencyExchangeServiceScrap.java
+++ CurrencyExchangeService/src/main/java/com/dipanjal/example/microservices/CurrencyExchangeService/service/CurrencyExchangeServiceScrap.java
@@ -3,34 +3,36 @@ package com.dipanjal.example.microservices.CurrencyExchangeService.service;
 import com.dipanjal.example.microservices.CurrencyExchangeService.exception.CurrencyExchangeException;
 import com.dipanjal.example.microservices.CurrencyExchangeService.props.CurrencyExchangeProps;
 import com.dipanjal.example.microservices.common.models.CurrencyExchange;
+import com.dipanjal.htmlunit.service.scrapper.ifaces.CurrencyExchangeScrapper;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Locale;
 
 @Slf4j
 @Service("currencyExchangeServiceScrap")
 @RequiredArgsConstructor
 public class CurrencyExchangeServiceScrap extends BaseService implements CurrencyExchangeService {
 
-//    private final CurrencyExchangeScrapper<BigDecimal> currencyExchangeScrapper;
+    private final CurrencyExchangeScrapper<BigDecimal> currencyExchangeScrapper;
     private final CurrencyExchangeProps props;
 
     @Override
     @CircuitBreaker(name = "retryServiceB", fallbackMethod = "currencyExchangeFallbackHandler")
     public CurrencyExchange fetchValue(String from, String to) throws CurrencyExchangeException {
-        return new CurrencyExchange(1L, "USD", "BDT", new BigDecimal(88), getLocalServerPort());
-//        try{
-//            BigDecimal exchangeValue = currencyExchangeScrapper.scrap(
-//                    String.format(props.getApi().getUrl(), from, to),
-//                    BigDecimal::new
-//            );
-//            return new CurrencyExchange(1L, from, to, exchangeValue, getLocalServerPort());
-//        }catch (IOException e){
-//            throw new CurrencyExchangeException(e.getMessage(), e);
-//        }
+        try{
+            BigDecimal exchangeValue = currencyExchangeScrapper.scrap(
+                    String.format(props.getApi().getUrl(), from.toUpperCase(Locale.ROOT), to.toUpperCase()),
+                    BigDecimal::new
+            );
+            return new CurrencyExchange(1L, from, to, exchangeValue, getLocalServerPort());
+        }catch (IOException e){
+            throw new CurrencyExchangeException(e.getMessage(), e);
+        }
     }
 
     private CurrencyExchange currencyExchangeFallbackHandler(Throwable throwable){
