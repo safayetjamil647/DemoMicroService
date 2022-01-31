b074b131e8f19fe30189e3776a02fe9ca13150f4
dipanjal <dipanjalmaitra@gmail.com>
2022-02-01 03:09:51 +0600

    Swagger UI Done

---
+++ CurrencyExchangeService/src/main/java/com/dipanjal/example/microservices/CurrencyExchangeService/config/OpenApiConfiguration.java
@@ -0,0 +1,34 @@
+package com.dipanjal.example.microservices.CurrencyExchangeService.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dipanjal
+ * @since 0.0.1
+ */
+
+@Configuration
+public class OpenApiConfiguration {
+
+    private final String SECURITY_TYPE = "BearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_TYPE))
+                .components(getSecurityComponents());
+    }
+
+    private Components getSecurityComponents() {
+        return new Components()
+                .addSecuritySchemes(SECURITY_TYPE, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
+    }
+}
